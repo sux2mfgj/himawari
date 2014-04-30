38a9f4515f9452b454c78120931cd009b101cc7f
himaaaatti <sux2mfgj@gmail.com>
2014-04-29 22:05:23 -0700

    delete code

--- segment.h
+++ segment.h
@@ -3,69 +3,6 @@
 
 #include"func.h"
 
-struct SEGMENT_DESCRIPTOR
-{
-    unsigned short limit_low, base_low;
-    unsigned char base_mid;
-    unsigned accessed                   :1;
-    unsigned segment_type               :3;
-    unsigned descriptor_type            :1;
-    unsigned descriptor_privilege_level :2;
-    unsigned present                    :1;
-
-    unsigned limit_high                 :4;
-
-    unsigned available                  :1;
-    unsigned code_segment_for_64bit     :1;
-    unsigned default_operand_size       :1;
-    unsigned granularity                :1;
-
-    unsigned char base_high;
-};
-
-// struct GATE_DISCRIPTOR{
-//     short offset_low, selector;
-//     char dw_count, access_right;
-//     short offset_high;
-// };
-struct GATE_DISCRIPTOR
-{
-    unsigned short offset_low;
-    unsigned short selector;
-
-    unsigned char unused;
-
-    unsigned gate_type                  :4;
-    unsigned storage_segment            :1;
-    unsigned descriptor_privilege_level :2;
-    unsigned present                    :1;
-
-    unsigned short offset_high;
-};
-
-inline void init_gdtidt(void);
-static void set_segmdesc(
-        struct SEGMENT_DESCRIPTOR *sd,
-        unsigned int limit,
-        unsigned int base,
-        unsigned char accessed,
-        unsigned char segment_type,
-        unsigned char descriptor_type,
-        unsigned char descriptor_privilege_level,
-        unsigned char present);
-
-
-static void set_gatedesc(
-        struct GATE_DISCRIPTOR *gd,
-        unsigned offset,
-        unsigned selector,
-        unsigned char gate_type,
-        unsigned char storage_segment,
-        unsigned char descriptor_privilege_level,
-        unsigned char present);
-
-inline void init_pic(void);
-
 #define GDT_ADDR 0x00270000
 #define IDT_ADDR 0x0026f800
 
@@ -123,6 +60,66 @@ inline void init_pic(void);
 #define PIC_IMR_MASK_IRQ6 0x40
 #define PIC_IMR_MASK_IRQ7 0x80
 #define PIC_IMR_MASK_IRQ_ALL 0xff
+
+
+struct SEGMENT_DESCRIPTOR
+{
+    unsigned short limit_low, base_low;
+    unsigned char base_mid;
+    unsigned accessed                   :1;
+    unsigned segment_type               :3;
+    unsigned descriptor_type            :1;
+    unsigned descriptor_privilege_level :2;
+    unsigned present                    :1;
+
+    unsigned limit_high                 :4;
+
+    unsigned available                  :1;
+    unsigned code_segment_for_64bit     :1;
+    unsigned default_operand_size       :1;
+    unsigned granularity                :1;
+
+    unsigned char base_high;
+};
+
+
+struct GATE_DISCRIPTOR
+{
+    unsigned short offset_low;
+    unsigned short selector;
+
+    unsigned char unused;
+
+    unsigned gate_type                  :4;
+    unsigned storage_segment            :1;
+    unsigned descriptor_privilege_level :2;
+    unsigned present                    :1;
+
+    unsigned short offset_high;
+};
+
+inline void init_gdtidt(void);
+static void set_segmdesc(
+        struct SEGMENT_DESCRIPTOR *sd,
+        unsigned int limit,
+        unsigned int base,
+        unsigned char accessed,
+        unsigned char segment_type,
+        unsigned char descriptor_type,
+        unsigned char descriptor_privilege_level,
+        unsigned char present);
+
+
+static void set_gatedesc(
+        struct GATE_DISCRIPTOR *gd,
+        unsigned offset,
+        unsigned selector,
+        unsigned char gate_type,
+        unsigned char storage_segment,
+        unsigned char descriptor_privilege_level,
+        unsigned char present);
+
+inline void init_pic(void);
 
 
 #endif
