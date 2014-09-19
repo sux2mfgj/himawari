487457c902da4d4186a895fc6d98a44b7dd7e0c9
Shunsuke Mie <sux2mfgj@gmail.com>
2014-09-19 21:11:26 +0900

    fix location io_sti

--- src/main.c
+++ src/main.c
@@ -1,12 +1,11 @@
-#include"graphic.h"
-#include"segment.h"
-#include"func.h"
-#include"multiboot.h"
-#include"k_memory.h"
-#include"lib.h"
-#include"interrupt_handler.h"
-#include"task.h"
-
+#include "graphic.h"
+#include "segment.h"
+#include "func.h"
+#include "multiboot.h"
+#include "k_memory.h"
+#include "lib.h"
+#include "interrupt_handler.h"
+#include "task.h"
 
 /* uint8_t stack[3][1024]; */
 uint32_t stack[1024];
@@ -15,12 +14,12 @@ void task1(void)
 {
     int i = 0;
     io_sti();
-/*     printf(TEXT_MODE_SCREEN_RIGHT, "0x%x", io_load_eflags()); */
+    /*     printf(TEXT_MODE_SCREEN_RIGHT, "0x%x", io_load_eflags()); */
     for (i = 0; i < 100000; i++) {
         printf(TEXT_MODE_SCREEN_RIGHT, "task1: %d", i);
     }
 
-    while(true){
+    while (true) {
         io_hlt();
     }
 }
@@ -33,7 +32,7 @@ void task2(void)
         printf(TEXT_MODE_SCREEN_RIGHT, "task2: %d", i);
     }
 
-    while(true){
+    while (true) {
         io_hlt();
     }
 }
@@ -46,9 +45,10 @@ void kernel_entry(uint32_t magic, MULTIBOOT_INFO *multiboot_info)
 
     init_gdtidt();
 
-     if(!init_memory(multiboot_info)){
-        //TODO: panic
-        printf(TEXT_MODE_SCREEN_RIGHT, "-----------------kernel panic-----------------");
+    if (!init_memory(multiboot_info)) {
+        // TODO: panic
+        printf(TEXT_MODE_SCREEN_RIGHT,
+               "-----------------kernel panic-----------------");
         io_hlt();
     }
 
@@ -57,40 +57,37 @@ void kernel_entry(uint32_t magic, MULTIBOOT_INFO *multiboot_info)
     init_inthandler();
 
     enable_paging();
+
+    void *label = &&vm_start;
+    goto *(label + VIRTUAL_KERNEL_ADDR);
+vm_start:
+
     io_sti();
+    /*     printf(TEXT_MODE_SCREEN_LEFT, "hello"); */
+    /*     printf(TEXT_MODE_SCREEN_LEFT, "mem_lower: %d(KB)",
+     * multiboot_info->mem_lower); */
+    /*     printf(TEXT_MODE_SCREEN_LEFT, "mem_upper: %d(KB)",
+     * multiboot_info->mem_upper); */
+    /*     printf(TEXT_MODE_SCREEN_LEFT, "mem_total: %d(KB)",
+     * (multiboot_info->mem_upper + multiboot_info->mem_lower + 1024)); */
 
-/*     void *label = &&vm_start; */
-/*     goto *(label+ VIRTUAL_KERNEL_ADDR); */
-/* vm_start: */
+    /*     set_task(0, NULL, NULL); */
+    /*     set_task(1, task1, stack[0]+1024); */
+    /*     set_task(2, task2, stack[1]+1024); */
 
+    /*     printf(TEXT_MODE_SCREEN_LEFT, "%x", &_kernel_end); */
+    /*     printf(TEXT_MODE_SCREEN_LEFT, "%x", &_kernel_start); */
 
-/*     printf(TEXT_MODE_SCREEN_LEFT, "hello"); */
-/*     printf(TEXT_MODE_SCREEN_LEFT, "mem_lower: %d(KB)", multiboot_info->mem_lower); */
-/*     printf(TEXT_MODE_SCREEN_LEFT, "mem_upper: %d(KB)", multiboot_info->mem_upper); */
-/*     printf(TEXT_MODE_SCREEN_LEFT, "mem_total: %d(KB)", (multiboot_info->mem_upper + multiboot_info->mem_lower + 1024)); */
-
-/*     set_task(0, NULL, NULL); */
-/*     set_task(1, task1, stack[0]+1024); */
-/*     set_task(2, task2, stack[1]+1024); */
-
-/*     printf(TEXT_MODE_SCREEN_LEFT, "%x", &_kernel_end); */
-/*     printf(TEXT_MODE_SCREEN_LEFT, "%x", &_kernel_start); */
-
-    for(;;){
+    for (;;) {
         io_hlt();
         /*    io_cli();  */
         if (keyboard_data_queue_check()) {
             /*             io_sti(); */
-        }
-        else {
+        } else {
             /*             task_switch_c(0, 1); */
             /*             printf(TEXT_MODE_SCREEN_RIGHT, "test"); */
             /*             print_array_status(); */
         }
     }
-
 }
-
-
-
 
