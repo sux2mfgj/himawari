104b49b32dbc36b6cc0be22cc325c6a120710ce2
himaaaatti <sux2mfgj@gmail.com>
2016-03-25 23:56:20 +0900

    restart himawari for uefi

--- src/cmd.gdb
+++
@@ -1,3 +0,0 @@
-target remote localhost:10000
-file kernel/himawari
-
--- src/include/string.h
+++
@@ -1,6 +0,0 @@
-#ifndef _INCLUDED_STRING_H_
-#define _INCLUDED_STRING_H_
-
-void h_strcpy(char* dest, const char* src);
-
-#endif
