31ab6c1b5810d520e3ccc1fd0827be4a84ca5c0f
himaaaatti <sux2mfgj@gmail.com>
2016-04-01 17:50:03 +0900

    getmemorymap

--- src/boot/main.c
+++ src/boot/main.c
@@ -1,6 +1,63 @@
 #include <efi.h>
 #include <efilib.h>
 
+
+
+EFI_STATUS get_memory_map(EFI_MEMORY_DESCRIPTOR* memory_map, uintmax_t* memory_map_size, uintmax_t* map_key)
+{
+
+    EFI_STATUS Status = EFI_SUCCESS;
+    Status = uefi_call_wrapper(BS->GetMemoryMap, 5, memory_map_size, NULL,
+                               NULL, NULL, NULL);
+    if (memory_map_size == 0) {
+        return Status;
+    }
+
+    Print(L"memmap size: %d\n", *memory_map_size);
+    uintmax_t pages;
+retry:
+
+    *memory_map_size += (1 << 12);
+    pages = (*memory_map_size / 0x1000);
+
+    Status = uefi_call_wrapper(BS->AllocatePages, 4, AllocateAnyPages, EfiLoaderData,
+                              pages, memory_map);
+
+    if(EFI_ERROR(Status)) {
+        return Status;
+    }
+
+    uintmax_t descriptor_size, descriptor_version;
+
+//typedef EFI_STATUS(EFIAPI * EFI_GET_MEMORY_MAP)
+//  (   IN OUT UINTN *MemoryMapSize,
+//      IN OUT EFI_MEMORY_DESCRIPTOR *MemoryMap,
+//      OUT UINTN *MapKey,
+//      OUT UINTN *DescriptorSize,
+//      OUT UINT32 *DescriptorVersion)
+
+    Status = uefi_call_wrapper(BS->GetMemoryMap, 5, memory_map_size, memory_map, map_key, &descriptor_size, &descriptor_version);
+    if(EFI_ERROR(Status)) {
+        if(Status == EFI_BUFFER_TOO_SMALL) {
+            
+
+//typedef EFI_STATUS(EFIAPI * EFI_FREE_PAGES)
+//  (   IN EFI_PHYSICAL_ADDRESS Memory,
+//      IN UINTN Pages)
+            Status = uefi_call_wrapper(BS->FreePages, 2, memory_map, pages);
+            if(EFI_ERROR(Status)) {
+                return Status;
+            }
+            goto retry; 
+        }
+        return Status;
+    }
+
+    Print(L"pages: %d\n", pages);
+
+    return EFI_SUCCESS;
+}
+
 EFI_STATUS
 EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
@@ -9,12 +66,56 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
     EFI_LOADED_IMAGE* LoadedImageParent;
     EFI_LOADED_IMAGE* LoadedImage;
     EFI_HANDLE Image;
-/*     CHAR16* Options = L"root=/dev/sda2 rootfstype=btrfs rw quiet splash"; */
     EFI_STATUS Status = EFI_SUCCESS;
 
     InitializeLib(ImageHandle, SystemTable);
     Print(L"Hello, EFI!\n");
+    Print(L"boot loader 0x%08x\n", ImageHandle);
 
+    EFI_MEMORY_DESCRIPTOR* memory_map;
+    uintmax_t memory_map_size, map_key;
+
+    Status = get_memory_map(memory_map, &memory_map_size, &map_key);
+    if(EFI_ERROR(Status)) {
+        Print(L"get_memory_map: %r\n", Status);
+        while(1) {
+
+        }
+        return Status;
+    }
+    Print(L"memory_map: 0x%08x\n", memory_map);
+
+    for (int i = 0; i < memory_map_size; ++i) {
+
+        switch (memory_map[i].Type) {
+            case EfiLoaderCode:
+                Print(L"1-");
+            case EfiLoaderData:
+                Print(L"2-");
+            case EfiBootServicesCode:
+                Print(L"3-");
+            case EfiBootServicesData:
+                Print(L"4-");
+            case EfiConventionalMemory:
+                if (memory_map[i].Attribute & EFI_MEMORY_WB) {
+                    Print(L"0x%08x, %d\n", memory_map[i].PhysicalStart,
+                            memory_map[i].NumberOfPages);
+                }
+                else {
+                    Print(L"\n");
+                    break;
+                }
+                /*             case EFI_ACPI_RECLAIM_MEMORY: */
+                /*                 break; */
+                /*             case EFI_ACPI_MEMORY_NVS: */
+                /*                 break; */
+                /*             case EFI_UNUSABLE_MEMORY: */
+                /*                 break; */
+            default: 
+                break;
+        }
+    }
+    /*
     Status = uefi_call_wrapper(
         BS->OpenProtocol, 6, ImageHandle, &LoadedImageProtocol,
         &LoadedImageParent, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
@@ -30,7 +131,6 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
         return EFI_INVALID_PARAMETER;
     }
 
-/*     uintptr_t kernel_base_addr = 0x00100000; */
 
     Status = uefi_call_wrapper(BS->LoadImage, 6, FALSE, ImageHandle,
             Path, NULL, 
@@ -51,13 +151,12 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
         FreePool(Path);
         return Status;
     }
-/*     LoadedImage->LoadOptions = Options; */
-/*     LoadedImage->LoadOptionsSize = */
-/*         (StrLen(LoadedImage->LoadOptions) + 1) * sizeof(CHAR16); */
 
     Status = uefi_call_wrapper(BS->StartImage, 3, Image, NULL, NULL);
     uefi_call_wrapper(BS->UnloadImage, 1, Image);
     FreePool(Path);
+    */
 
+    while(1){}
     return EFI_SUCCESS;
 }
--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -1,8 +1,6 @@
 #include <efi.h>
 #include <efilib.h>
 
-/* #include <EfiTypes.h> */
-
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -14,7 +12,8 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
     uintmax_t memory_map_size;
 
     InitializeLib(ImageHandle, SystemTable);
-    Print(L"0x%08x\n", efi_main);
+    Print(L"efi_main: 0x%08x\n", efi_main);
+    Print(L"0x%08x\n", ImageHandle);
 
     status = uefi_call_wrapper(BS->GetMemoryMap, 5, &memory_map_size, NULL,
                                NULL, NULL, NULL);
@@ -25,36 +24,56 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
     EFI_MEMORY_DESCRIPTOR* memory_map;// = NULL;
     uintmax_t map_key, descriptor_size, descriptor_version;
 
-    status = uefi_call_wrapper(BS->AllocatePool, 4, EfiBootServicesData,
-                               memory_map_size, memory_map);
+
+    Print(L"memory_map_size: %d\n", memory_map_size);
+    status = uefi_call_wrapper(BS->AllocatePool, 4, EfiLoaderData,
+                               memory_map_size, (void **)memory_map);
+/*     status = uefi_call_wrapper(BS->AllocatePool, 4, EfiBootServicesData, */
+/*                                memory_map_size, memory_map); */
     if (EFI_ERROR(status)) {
+        Print(L"allocate pool: %r\n", status);
         return status;
     }
 
     status =
-        uefi_call_wrapper(BS->GetMemoryMap, 5, &memory_map_size, memory_map,
+        uefi_call_wrapper(BS->GetMemoryMap, 5, &memory_map_size, (void *)memory_map,
                           &map_key, &descriptor_size, &descriptor_version);
 
     if (EFI_ERROR(status)) {
+        Print(L"get memory map: %r\n", status);
+        while (1) {
+            
+        }
         return status;
     }
 
-    Print(L"0x%08x\n", memory_map);
-    Print(L"%d\n", memory_map_size);
+    Print(L"memory_map addr 0x%08x\n", (uintptr_t)&memory_map[0]);
+    Print(L"size %d\n", memory_map_size);
 
+    uintptr_t max_start = 0;
+    Print(L"aaa 0x%08x\n", max_start);
     for (int i = 0; i < memory_map_size; ++i) {
 
         switch (memory_map[i].Type) {
             case EfiLoaderCode:
+                Print(L"1-");
             case EfiLoaderData:
+                Print(L"2-");
             case EfiBootServicesCode:
+                Print(L"3-");
             case EfiBootServicesData:
+                Print(L"4-");
             case EfiConventionalMemory:
+                if(max_start < memory_map[i].PhysicalStart) {
+                    max_start = memory_map[i].PhysicalStart;
+                }
+
                 if (memory_map[i].Attribute & EFI_MEMORY_WB) {
                     Print(L"0x%08x, %d\n", memory_map[i].PhysicalStart,
                             memory_map[i].NumberOfPages);
                 }
                 else {
+                    Print(L"\n");
                     break;
                 }
 /*             case EFI_ACPI_RECLAIM_MEMORY: */
@@ -64,11 +83,31 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 /*             case EFI_UNUSABLE_MEMORY: */
 /*                 break; */
             default: 
+                if(max_start < memory_map[i].PhysicalStart) {
+                    max_start = memory_map[i].PhysicalStart;
+                }
                 break;
         }
+    }
+    Print(L"max_start 0x%08x\n", max_start);
 
+/*     status = uefi_call_wrapper(BS->AllocatePool, 4, EfiBootServicesData, */
+/*                                memory_map_size, memory_map); */
+/*     if (EFI_ERROR(status)) { */
+/*         Print(L"allocate pool: %r\n", status); */
+/*         return status; */
+/*     } */
+
+
+    status = uefi_call_wrapper(BS->ExitBootServices, 2, ImageHandle, map_key);
+    if(EFI_ERROR(status)) {
+        Print(L"exit: %r\n", status);
+        while(1){}
+        return status;
     }
 
-    while (1)
-        ;
+    while (1){}
+        
+    return status;
 }
+
