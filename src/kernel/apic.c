c8d25c7aac1af0d86f7e26fd0cfb70f729ddb834
Shunsuke Mie <sux2mfgj@gmail.com>
2019-08-22 00:55:13 +0900

    check a status of local apic.

--- src/kernel/apic.c
+++ src/kernel/apic.c
@@ -1,18 +1,37 @@
 #include "apic.h"
 #include "cpuid.h"
+#include "msr.h"
 
 // 10.4.2 Presence of the Local APIC
-bool is_presence_local_apic(void)
+static bool is_presence_local_apic(void)
 {
     uint32_t edx;
     cpuid(1, &edx);
 
+    // TODO remove the magic number
     if(edx & 0x100)
     {
         return true;
     }
 
     return false;
+}
+
+// 10.4.3 Enabling or Disabling the Local APIC
+static bool is_enable_local_apic(void)
+{
+    uint64_t apic_base = read_msr(IA32_APIC_BASE);
+    // TODO remove the magic number
+    return !!(apic_base & 0x800);
+}
+
+static void enable_local_apic(void)
+{
+    uint64_t apic_base = read_msr(IA32_APIC_BASE);
+    apic_base |= 0x800;
+    uint32_t low = (uint32_t)apic_base;
+    uint32_t high = apic_base >> 32;
+    write_msr(IA32_APIC_BASE, high, low);
 }
 
 bool init_apic(void)
@@ -24,6 +43,14 @@ bool init_apic(void)
     {
         return false;
     }
+
+    result = is_enable_local_apic();
+
+    if(!result)
+    {
+        enable_local_apic();
+    }
+
 
     //TODO
 
--- src/kernel/msr.h
+++ src/kernel/msr.h
@@ -2,5 +2,9 @@
 
 #include <stdint.h>
 
+enum {
+    IA32_APIC_BASE = 0x1b
+};
+
 uint64_t read_msr(uint32_t msr);
 void write_msr(uint32_t msr, uint32_t high, uint32_t low);
