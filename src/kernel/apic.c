ee8885846534b9f99d4d4b5733be9aeb6a758651
Shunsuke Mie <sux2mfgj@gmail.com>
2019-08-22 01:06:52 +0900

    get local apic location.

--- src/kernel/apic.c
+++ src/kernel/apic.c
@@ -1,6 +1,15 @@
 #include "apic.h"
 #include "cpuid.h"
 #include "msr.h"
+#include "utils.h"
+
+enum IA32_APIC_BASE_BITS {
+    IA32_APIC_BASE_BSP  = 0x100,
+    IA32_APIC_BASE_GLOBAL_ENABLE = 0x800,
+    IA32_APIC_BASE_APIC_BASE = ~0xfff,
+};
+
+static uintptr_t bsp_local_apic_base;
 
 // 10.4.2 Presence of the Local APIC
 static bool is_presence_local_apic(void)
@@ -22,16 +31,27 @@ static bool is_enable_local_apic(void)
 {
     uint64_t apic_base = read_msr(IA32_APIC_BASE);
     // TODO remove the magic number
-    return !!(apic_base & 0x800);
+    return !!(apic_base & IA32_APIC_BASE_GLOBAL_ENABLE);
 }
 
 static void enable_local_apic(void)
 {
     uint64_t apic_base = read_msr(IA32_APIC_BASE);
-    apic_base |= 0x800;
+    apic_base |= IA32_APIC_BASE_GLOBAL_ENABLE;
     uint32_t low = (uint32_t)apic_base;
     uint32_t high = apic_base >> 32;
     write_msr(IA32_APIC_BASE, high, low);
+}
+
+static void setup_bsp_local_apic_base(void)
+{
+    uint64_t apic_base = read_msr(IA32_APIC_BASE);
+    if(!(apic_base & IA32_APIC_BASE_BSP))
+    {
+        assert(false, "not yet implemented for MP system");
+    }
+
+    bsp_local_apic_base = apic_base & IA32_APIC_BASE_APIC_BASE;
 }
 
 bool init_apic(void)
@@ -51,6 +71,7 @@ bool init_apic(void)
         enable_local_apic();
     }
 
+    setup_bsp_local_apic_base();
 
     //TODO
 
