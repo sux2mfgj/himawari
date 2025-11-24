68e65e117765a2460246c831bf29bf172035b522
Shunsuke Mie <sux2mfgj@gmail.com>
2025-11-24 16:52:42 +0900

    string: introduce a memcpy

--- inc/hm/string.h
+++ inc/hm/string.h
@@ -6,3 +6,4 @@
 
 bool memcmp(void *a, void *b, size_t size);
 void memset(void *a, uint8_t byte, size_t size);
+void memcpy(void *dst, void *src, size_t size);
--- src/string.c
+++ src/string.c
@@ -19,3 +19,11 @@ void memset(void *a, uint8_t byte, size_t size) {
   for (int i = 0; i < size; i++)
     ap[i] = byte;
 }
+
+void memcpy(void *dst, void *src, size_t size) {
+  uint8_t *src_p = (uint8_t *)src;
+  uint8_t *dst_p = (uint8_t *)dst;
+
+  for (int i = 0; i < size; i++)
+    dst_p[i] = src_p[i];
+}
